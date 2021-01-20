8b49414ddde466d9d8c15fb1cb86788bf69b73b8
shrued <shrutisara10@gmail.com>
2021-01-20 19:11:40 +0530

    Docker + update

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def hello():
     return jsonify({"Message": "Hola!"})
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port = 5001, debug=True)
