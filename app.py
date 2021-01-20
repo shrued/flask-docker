4e0796db7cc86b92efd665ee7d85aaa6757babec
shrued <shrutisara10@gmail.com>
2021-01-20 19:11:34 +0530

    Update

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return jsonify({"Message": "How you doin'"})
+    return jsonify({"Message": "Hola!"})
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
